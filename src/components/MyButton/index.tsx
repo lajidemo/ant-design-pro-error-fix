27d60ab621ed85cc72a56a5956df1e64f4485d7e
zhuangjiaren <zhuang.jiaren@xyb2b.com>
2026-01-22 18:38:58 +0800

    feat: 更新

---
+++ src/components/MyButton/index.tsx
@@ -0,0 +1,24 @@
+import { Button, type ButtonProps, ConfigProvider } from 'antd';
+import { createStaticStyles, createStyles } from 'antd-style';
+
+const styles = createStaticStyles(({ css, cssVar }) => {
+  console.log('cssVar.colorPrimary', cssVar.colorPrimary);
+  return {
+    container: css`
+    position: absolute;
+    inset: 0;
+    overflow: hidden;
+    background-color: ${cssVar.colorPrimary};
+  `,
+  };
+});
+
+const MyButton = (props: ButtonProps) => {
+  return (
+    <div className={styles.container}>
+      <Button {...props}>{props.children}</Button>
+    </div>
+  );
+};
+
+export default MyButton;
--- src/pages/Welcome.tsx
+++ src/pages/Welcome.tsx
@@ -1,7 +1,8 @@
 import { PageContainer } from '@ant-design/pro-components';
 import { useModel } from '@umijs/max';
-import { Card, theme } from 'antd';
+import { Card, ConfigProvider, theme } from 'antd';
 import React from 'react';
+import MyButton from '@/components/MyButton';
 
 /**
  * 每个单独的卡片，为了复用样式抽成了组件
@@ -19,67 +20,76 @@ const InfoCard: React.FC<{
   const { token } = useToken();
 
   return (
-    <div
-      style={{
-        backgroundColor: token.colorBgContainer,
-        boxShadow: token.boxShadow,
-        borderRadius: '8px',
-        fontSize: '14px',
-        color: token.colorTextSecondary,
-        lineHeight: '22px',
-        padding: '16px 19px',
-        minWidth: '220px',
-        flex: 1,
+    <ConfigProvider
+      theme={{
+        token: {
+          colorPrimary: 'red',
+        },
       }}
     >
       <div
         style={{
-          display: 'flex',
-          gap: '4px',
-          alignItems: 'center',
-        }}
-      >
-        <div
-          style={{
-            width: 48,
-            height: 48,
-            lineHeight: '22px',
-            backgroundSize: '100%',
-            textAlign: 'center',
-            padding: '8px 16px 16px 12px',
-            color: '#FFF',
-            fontWeight: 'bold',
-            backgroundImage:
-              "url('https://gw.alipayobjects.com/zos/bmw-prod/daaf8d50-8e6d-4251-905d-676a24ddfa12.svg')",
-          }}
-        >
-          {index}
-        </div>
-        <div
-          style={{
-            fontSize: '16px',
-            color: token.colorText,
-            paddingBottom: 8,
-          }}
-        >
-          {title}
-        </div>
-      </div>
-      <div
-        style={{
+          backgroundColor: token.colorBgContainer,
+          boxShadow: token.boxShadow,
+          borderRadius: '8px',
           fontSize: '14px',
           color: token.colorTextSecondary,
-          textAlign: 'justify',
           lineHeight: '22px',
-          marginBottom: 8,
+          padding: '16px 19px',
+          minWidth: '220px',
+          flex: 1,
         }}
       >
-        {desc}
+        <MyButton type="primary">按钮</MyButton>
+        <div
+          style={{
+            display: 'flex',
+            gap: '4px',
+            alignItems: 'center',
+          }}
+        >
+          <div
+            style={{
+              width: 48,
+              height: 48,
+              lineHeight: '22px',
+              backgroundSize: '100%',
+              textAlign: 'center',
+              padding: '8px 16px 16px 12px',
+              color: '#FFF',
+              fontWeight: 'bold',
+              backgroundImage:
+                "url('https://gw.alipayobjects.com/zos/bmw-prod/daaf8d50-8e6d-4251-905d-676a24ddfa12.svg')",
+            }}
+          >
+            {index}
+          </div>
+          <div
+            style={{
+              fontSize: '16px',
+              color: token.colorText,
+              paddingBottom: 8,
+            }}
+          >
+            {title}
+          </div>
+        </div>
+        <div
+          style={{
+            fontSize: '14px',
+            color: token.colorTextSecondary,
+            textAlign: 'justify',
+            lineHeight: '22px',
+            marginBottom: 8,
+          }}
+        >
+          {desc}
+        </div>
+        <a href={href} target="_blank" rel="noreferrer">
+          了解更多 {'>'}
+        </a>
       </div>
-      <a href={href} target="_blank" rel="noreferrer">
-        了解更多 {'>'}
-      </a>
-    </div>
+    </ConfigProvider>
   );
 };
 
